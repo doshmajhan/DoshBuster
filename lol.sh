a9f9019e5e538b3a4f0d3cfe42abde728e55c52c
dosh <cpc1007@g.rit.edu>
2016-02-06 03:00:37 +0000

    adding threading now

--- lol.sh
+++ lol.sh
@@ -1,5 +1,16 @@
 filename='test.list'
-site='ctf.arch-cloud.com/var/'
+output='out.file'
+site='ctf.arch-cloud.com/'
 agent='Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.82 Safari/537.36 OPR/35.0.2066.37'
-#curl -A $agent $site | echo
-wget --header="Accept: text/html" --user-agent=$agent $site | echo
+
+echo Starting Doshbuster
+while read p; do
+    temp=$site$p
+    if [ "$(wget --header="Accept: text/html" --user-agent=$agent "$temp" 2>&1 | grep -c '200\|OK')" -gt 0 ]; then
+        echo found $p >> $output
+    fi
+
+    echo $p
+done < $filename
+
+cat $output
